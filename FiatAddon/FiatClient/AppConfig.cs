490dd86b94ad32df3369b567ee381500c8861a8b
Blueion <128919662+Blueion76@users.noreply.github.com>
2024-11-15 15:48:21 -0600

    Update AppConfig.cs

--- FiatAddon/FiatClient/AppConfig.cs
+++ FiatAddon/FiatClient/AppConfig.cs
@@ -2,7 +2,7 @@ using System.ComponentModel.DataAnnotations;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 
-namespace FiatUconnect;
+namespace FCAUconnect;
 
 public record AppConfig
 {
